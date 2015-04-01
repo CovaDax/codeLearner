126d0842f5ce6cf5257cff9a1e8224b5ba8f346d
Andrew Gari <covadax.software@gmail.com>
2015-04-01 18:27:32 -0400

    Added quiz functionality

--- app/src/main/java/edu/fgcu/codelearner/VariablesQuiz.java
+++ app/src/main/java/edu/fgcu/codelearner/VariablesQuiz.java
@@ -1,44 +1,28 @@
 package edu.fgcu.codelearner;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ExpandableListView;
-import android.widget.ListView;
 
 
-public class Variables extends ActionBarActivity {
+public class VariablesQuiz extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_variables);
+        setContentView(R.layout.activity_variables_quiz);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Variables");
-
-        Button varQuiz =(Button)findViewById(R.id.button);
-        varQuiz.setOnClickListener(new View.OnClickListener()
-        {   public void onClick(View v)
-            {
-                Intent intent = new Intent(getApplicationContext(), VariablesQuiz.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
+        actionBar.setTitle("Variables Quiz");
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_variables, menu);
+        getMenuInflater().inflate(R.menu.menu_variables_quiz, menu);
         return true;
     }
 
